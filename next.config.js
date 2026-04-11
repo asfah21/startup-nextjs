1d818e6224b33261c2a68922cc906b9b53be6906
Muh Al Asfahani <asfah21@gmail.com>
2026-04-11 14:54:57 +0800

    [IMG] Add support Img resolution

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days cache to reduce VPS load
-    deviceSizes: [640, 768, 1024, 1280, 1920], // Reduce size generated
+    deviceSizes: [640, 768, 1024, 1280, 1366, 1440, 1600, 1920], // Added more specific sizes to prevent oversized images (1796x) on 1440p displays
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     remotePatterns: [
       {
